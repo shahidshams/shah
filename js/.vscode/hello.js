858af215cc24c97b14364c90379119afb7509131
CN107 <cn107@CN0107.local>
2019-11-15 16:49:28 +0000

    pro

---
+++ js/.vscode/hello.js
@@ -0,0 +1,368 @@
+
+
+/*let someonename = "sam";
+let samage=",43";
+let samecolour= ",green";
+console.log("age of same "+ someonename, "he is "+ samage,"year old, "+"he like green colour"+samecolour);
+
+
+ let breakfast = " coffee";
+ let lunch = "chicken burger";
+ let dinner = "lentels";
+
+ let tomorrow = "fish and chips";
+ console.log ("at morning breafast was just" + breakfast, "now in lunch i ate" + lunch
+ ,"and dinner gonna eat"+dinner,"tomorrow will eat"+tomorrow);
+*/
+/*
+let space = "   |   |";
+let space2= " x | 0 |";
+let space3= " 0 |   |";
+let space4= " x | x |";
+let space5= "___________"
+
+ console.log (space);
+ console.log (space3);
+ console.log (space);
+ console.log (space5);
+ console.log (space);
+ console.log (space4);
+ console.log (space);
+ console.log (space5);
+ console.log(space);
+ console.log (space2);
+ console.log(space);
+ console.log (space5); */
+
+ /*if (1 ==="1") {
+     console.log(true);
+ }
+ else {
+     console.log (false);
+ }
+ */
+
+
+ /*let place ="manc";
+ let weather= "cloudy";
+
+ if (place== "manc" &&weather == "sunny"){
+     console .log( "check again");
+ }
+
+ else if ( place== "manc"&&weather == "rain"){
+     console.log("obvs");
+ }
+ 
+ else {
+     console.log("what it isn't raining?");
+ }*/
+
+ /*let blackcar= "toyota";
+ let redcar= "BMW";
+
+ if (blackcar=="toyota" && redcar=="BMW"){
+     console.log("stop them");
+ }
+ else if (blackcar=="BMW"&&redcar=="toyota"){
+     console.log("no you are wrong");
+ }
+ else{
+     console.log("all of them your showed are wrong");
+ }*/
+ /*let morning="goodmorning";
+ let afternoon="goodafternoon";
+ let evening= "goodevning";
+ let night="goodnight";
+ if (morning=="goodmornin"){
+     console.log("yes this is morning");
+ }
+ else if (evening=="night"){
+     console.log("this is evening");
+ }
+ else if ( night=="how "){
+     console.log("this is night");
+ }
+ else if ( morning == " ")
+ else{
+     console.log("this not possible");
+ }*/
+
+ /*let password="1234";
+ if (password.length<8){
+     console.log("good");
+ }
+ else ("wrong");*/
+
+/*let num=56;
+if (num/3){
+    console.log("correct");
+}
+ else("number is not divisibleby 3")*/
+
+ /*let num=20;
+ if (num/3){
+     console.log("fizz");
+ }
+ else if(num/5){
+     console.log("buzz");
+ }
+ else if(num/3&&num/5){
+     console.log("fizz buzz");
+ }*/
+
+ /*4
+ num=palindrome;
+ 
+ palindro;
+let num=10;
+ 
+ if (num==palindrome){
+     console.log(palindrome);
+ }
+ else(not);*/
+
+ 1
+
+ /*let ordercount =0
+ const takeorder=(topping)=> {
+     console.log ('pizza with $(tooping)');
+     ordercount++;
+ }
+ takeorder("pineapple");*/
+
+ /*const multiply=(num1,num2)=>{
+     return num1*num2
+ }*/
+
+/* let songs=[
+     "1 colli - sorry",
+     "2 ella - hello",
+     "3 sam - kori"
+ ];
+songs.pop();
+console.log(songs);*/
+
+/*let website=[
+    "calling.com",
+    "hope.co.uk",
+    "mytime.com"
+];
+website.push("dingo.com");
+website.push("0la.com")
+console.log(website);*/
+
+/*let website=[
+    "calling.com",
+    "mith.com",
+    "simon.com",
+];
+website.splice();
+console.log(website);*/
+
+// let ordercount = 0
+//  const takeorder=(topping)=> {
+//      console.log ('pizza with $(tooping)');
+//      ordercount++;
+//  }
+//  takeorder("pineapple");
+
+// const multiply=(num1,num2)=>{
+//      return num1*num2;
+// }
+
+// console.log(multiply(2,3));
+
+// let film=[
+//     "1 colli - sorry",
+//     "2 ella - hello",
+//     "3 sam - kori"
+// ];
+// console.log(film);
+// /let film=[
+//     "1 mike-romer",
+//     "2 colli-sporing"
+// ]
+// console.log(film[1]);
+
+// let coffeeorder=[
+//     "1 dan-coffee",
+//     "2 mat-tea",
+//     "3 sam-juice"
+// ]
+// coffeeorder.p0p();
+// console.log (coffeeorder);
+
+// let coffeeorder=[
+//     "1 dan-coffee",
+//     "2 mat-tea",
+//     "3 sam-juice"
+// ]
+// coffeeorder.splice();
+// console.log (coffeeorder);
+
+
+// let myarray=[
+//     "alleshka",
+//     "dan",
+//     "ashlay",
+//     "kori"
+// ]
+//     myarray.sort();
+// console.log(myarray);
+
+// let myarray=[
+//     "dubble",
+//     "tripple"
+// ]
+// myarray.toString();
+// console.log(myarray);
+
+// let user=[
+//     "codenation"
+// ]
+// user.sortalphabits();
+// console.log(user);
+
+// let sandwich=[
+//     "chees",
+//     "special",
+//     "chilli",
+//     "hot chilli"
+// ]
+// console.log(sandwich);
+
+// let my=[
+//     "laptop",
+//     "house",
+//     "bag"
+// ]
+// my.push("pen");
+// console.log(my);
+
+//console.log(math.random())
+
+// let my=[
+//     "pen",
+//     "book",
+//     "mobile"
+// ]
+// for (let i=0;i<my.length;i++){
+//     console.log(my[i]);
+// }
+
+// let num=[
+//     "1",
+//     "2",
+//     "3"
+// ]
+// for (let i=0;i<num.length;i++){
+//     console.log(num[i]);
+// }
+
+
+// let cads=["dimond","spade","heart","club"];
+// let currentcard="spade";
+// while(currentcard!="spade"){
+//     console.log(currentcard);
+//     currentcard=card[Math.floor(math.random()*4)];
+// }
+// console.log(currentcard);
+
+
+//  let num=["1","2","3","4"];
+//  let num2="2",
+//  while(num2!="2"){
+//      console.log(num2);
+     
+//  }
+ 
+
+
+// let film=["vikings","cold winter","lucy","build back","move"];
+//   console.log("boo, is not this one,try again");
+//   let film3="lucy";
+//   while(film3!="lucy"){console.log("yea this is");
+//   console.log("boo, is not this one,try again");
+//  }
+
+// let film=["vikings","coldwinter","lucy","build back","move"];
+//    let film3="lucy";
+//    while(film3!="lucy"){console.log("yes this is");
+//  console.log("boo,is not this one ,try again");
+// }
+
+// const person={
+// name :"shahid",
+// value:100,
+// age:40,
+// }
+// console.log(person["name"]);
+
+// const alarm={
+//     weekendalarm : "no alarm needed",
+//     weekdaysalarm: "get up at 7am",
+// }
+// let day="monday"
+// let alarm
+// if (day==saturday||sunday){alarm=weekendalarm};
+// console.log(alarm)
+
+// const songs={
+//     fa
+
+// }
+
+// const song={
+//     favouritsong :"song",
+//     value:100,
+//     age:40,
+//     sayHi() { 
+//         console.log("my name is cham")
+
+//     }
+// }
+// song.sayHi()
+
+// let person={
+//     name:"shahid",
+//     age:80,
+//     job:"school",
+//     value:100
+// }
+// console.log(person)
+
+// let dansNumber = 1
+
+// console.log(dansNumber++)
+
+// console.log(dansNumber)
+// let books={
+//     neer:"kokeck",
+//     ponk:"koli",
+//     shome:"kamili"
+// }
+// console.log(books)
+// let jo=10
+// console.log(jo++)
+// console.log(jo++)
+
+// var str = 'Life, the universe and everything. Answer:';
+  
+// console.log(str + ' ' + str.length);
+
+
+
+    
+function toSeconds(str) {
+    var pieces = str.split(":");
+    var result = Number(pieces[0]) * 60 + Number(pieces[1]);
+    return(result.toFixed(3));
+}
+
+console.log(toSeconds("1:29.460"));
+
+let hum="ldk";
+console.log(hum);
+
+
